274d4d0ba4dc418fae8a47c292f31cefd4d4eb36
amedina9 <amedina9@sycamores.indstate.edu>
2024-06-09 01:01:23 -0400

    add club page with badges

--- src/pages/Clubs.js
+++ src/pages/Clubs.js
@@ -1,6 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import zgBadge from "../assets/images/clubs/zg.png"
+import emBadge from "../assets/images/clubs/emperors.png"
+import intBadge from "../assets/images/clubs/inter.png"
 import "../App.css";
+import "./Clubs.css"
 
 export default function Clubs() {
-  return <h1 className="clubs">CLUBS</h1>;
+
+  return (
+    <>
+      <div className="clubs">
+        <div className="clubs-container">
+          <h1>CLUBS</h1>
+          <ul className="clubs-list">
+            <li className="clubs-item">
+              <Link to="/clubs" className="clubs-links">
+                <img src={zgBadge} alt="Zero Gravity" className="clubs-zg" />
+                Zero Gravity
+              </Link>
+            </li>
+            <li className="clubs-item">
+              <Link to="/clubs" className="clubs-links">
+                <img src={emBadge} alt="Emperors Alliance" className="clubs-emp" />
+                Emperors Alliance
+              </Link>
+            </li>
+            <li className="clubs-item">
+              <Link to="/clubs" className="clubs-links">
+                <img src={intBadge} alt="Inter Marine" className="clubs-int" />
+                Inter Marine
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
